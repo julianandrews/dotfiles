b1f365b196bf0f826b0b13f195707bd1bdb08c55
Julian Andrews <jandrews271@gmail.com>
2015-06-22 19:41:20 -0600

    Fancy up install script

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-mkdir -p ~/.config/xfce4/terminal
-ln -s "$DIR/.config/i3" ~/.config/
-ln -s "$DIR/.config/i3status" ~/.config/
-ln -s "$DIR/.config/xfce4/terminal/terminalrc" ~/.config/xfce4/terminal/terminalrc
+dotfile_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -s "$DIR/.virtualenvs/postactivate" ~/.virtualenvs/
-ln -s "$DIR/bin" ~/
-ln -s "$DIR/.bashrc" ~/
-ln -s "$DIR/.vimrc" ~/
-ln -s "$DIR/.vim" ~/
+function backup_and_link {
+  backup_dir="$dotfile_dir/backup"
+  mkdir -p "$backup_dir"
+  source_file="$dotfile_dir/$1"
+  target_file="$HOME/$1"
+  if [ -L "$target_file" ]; then unlink "$target_file"; fi
+  if [ -e "$target_file" ]; then mv "$target_file" "$backup_dir"; fi
+  ln -s "$source_file" "$target_file"
+}
+
+backup_and_link .config/i3
+backup_and_link .config/i3status
+backup_and_link .config/xfce4/terminal/terminalrc
+backup_and_link .virtualenvs/postactivate
+backup_and_link bin
+backup_and_link .bashrc
+backup_and_link .vimrc
+backup_and_link .vim
