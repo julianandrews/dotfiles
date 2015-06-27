012550af5818b2c652e9d26ed0f770ba2094b67e
Julian Andrews <jandrews271@gmail.com>
2015-06-27 05:44:51 -0600

    Fix gmail status to actually work.

--- .config/i3status/custom_status.sh
+++ .config/i3status/custom_status.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-EMAILS=('jandrews271@gmail.com','#00FF00' 'jandrews@fusionbox.com','#0000FF')
-OLDIFS=$IFS
+if [ $HOSTNAME == 'orpheus' ]
+then
+  EMAIL='jandrews@fusionbox.com'
+else
+  EMAIL='jandrews271@gmail.com'
+fi
+
+EMAIL_COLOR='#OOFFOO'
 
 function gmail {
   count=$(gmail-count -u $1)
   if [ "$count" == "0" ]; then
-    color="#585858"
+    color='#585858'
   elif [ "$count" == "?" ]; then
-    color="#FF0000"
+    color='#FF0000'
   else
     color=$2
   fi
   echo "{\"name\":\"$1\",\"color\":\"$color\",\"full_text\":\"✉ $count\"}"
 }
 
-i3status | (read line && echo $line && read line && echo $line && read line && echo $line && while :
-do
-  read line
-  line=${line:2}
-  for args in ${EMAILS[@]}; do
-    IFS=','
-    line="$(gmail $args),$line"
-    IFS=$OLDIFS
+function prepend_gmail {
+  echo "[$(gmail $EMAIL $EMAIL_COLOR),${1:1}"
+}
+
+function rewrite_stream {
+  read line && echo $line && read line && echo $line
+  read line && echo "$(prepend_gmail "$line")"
+  while :
+  do
+    read line && echo ",$(prepend_gmail "${line:1}")"
   done
-  echo ",[$line"
-done)
+}
+
+i3status | rewrite_stream
