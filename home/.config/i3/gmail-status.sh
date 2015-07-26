39cb782c322e8d123e1682eb67e1a66973b3486d
Julian Andrews <jandrews271@gmail.com>
2015-07-26 12:28:13 -0600

    Minor tidy up to gmail-status

--- home/.config/i3/gmail-status.sh
+++ home/.config/i3/gmail-status.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-[ "$BLOCK_BUTTON" = "1" ] && xdg-open https://inbox.google.com
+[ "$BLOCK_BUTTON" = 1 ] && xdg-open https://inbox.google.com
 
 case $(hostname) in
   orpheus)     email='jandrews@fusionbox.com' ;;
   *)           email='jandrews271@gmail.com' ;;
 esac
 
-count=$(~/bin/gmail_count "$email")
+full_text=$(~/bin/gmail_count "$email")
 
-case $count in
+case $full_text in
   ''|*[!0-9]*) color=\#FF0000 ;;
   0)           color=\#888888 ;;
   *)           color=\#00FF00 ;;
 esac
 
-echo "$count"
-echo
+echo "$full_text"
+echo "$short_text"
 echo "$color"
