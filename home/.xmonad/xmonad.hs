3fc12ccd20d472bab4cd9dd9012ec5648740a302
Julian Andrews <jandrews@fusionbox.com>
2016-01-24 19:00:55 -0700

    Add vertical layout for XMonad

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -85,16 +85,22 @@ purpleTheme =
 focusedColor = "#81206D"
 unfocusedColor = "#51A39D"
 
-myLayout = myMain ||| myFullscreenTabbed
+myLayout = myHorizontal ||| myVertical ||| myFullscreenTabbed
   where
     myTabbed = tabbed shrinkText (theme purpleTheme)
     myFullscreenTabbed = noBorders . renamed [Replace "Tabbed"] . fullscreenFull
       $ myTabbed
-    myMain = 
-      mkToggle (single MIRROR) . renamed [Replace "Main"] . smartBorders
+    myHorizontal = 
+      renamed [Replace "Horizontal"] . smartBorders
         $ layoutN 1 (relBox 0 0 (3/5) 1) (Just $ relBox 0 0 1 1) myTabbed
         $ layoutN 1 (relBox (3/5) 0 1 (1/2)) (Just $ relBox (3/5) 0 1 1) myTabbed
         $ layoutAll (relBox (3/5) (1/2) 1 1) myTabbed
+    myVertical =
+      renamed [Replace "Vertical"] . smartBorders
+        $ layoutN 1 (relBox 0 0 1 (3/5)) (Just $ relBox 0 0 1 1) myTabbed
+        $ layoutN 1 (relBox 0 (3/5) (1/2) 1) (Just $ relBox 0 (3/5) 1 1) myTabbed
+        $ layoutAll (relBox (1/2) (3/5) 1 1) myTabbed
+        
 
 myManageHook = composeAll [
     role =? "gimp-image-window" --> (ask >>= doF . W.sink),
