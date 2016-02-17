f7e3fd0c9fb78dfe0fef5774d1402f253e3e6cc2
Julian Andrews <jandrews@fusionbox.com>
2016-02-17 10:07:28 -0700

    Add noise to screen lock because security
    Ok, honestly, because it seems cool. My hard drive isn't encrypted and
    I'm pretty sure it's less trouble to use rubber hose cryptography than
    to attempt to reverse my image processing to see whatever's on my
    screen.

--- home/.local/bin/screen-lock.sh
+++ home/.local/bin/screen-lock.sh
@@ -2,5 +2,5 @@
 
 image_file=/tmp/screen_lock.png
 scrot -z "$image_file"
-mogrify -scale 5% -scale 2000% "$image_file"
+mogrify -noise 5x5 -scale 5% -scale 2000% "$image_file"
 i3lock -e -i "$image_file"
