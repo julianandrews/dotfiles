4802dabbd748b80fff0292b6d3da3bfda8e17503
Julian Andrews <jandrews@fusionbox.com>
2016-03-03 07:51:32 -0700

    Make dotfiles install script properly backup files

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ dotfile_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 function backup_and_link {
   backup_dir="$dotfile_dir/backup-$(date +%Y-%m-%d-%H%M)"
-  source_file="$dotfile_dir/home/$1"
-  target_file="$HOME/$1"
-  mkdir -p $(dirname $target_file)
+  source_file="$1"
+  rel_path=$(echo $1 | sed 's|'"$dotfile_dir/home"'/||')
+  target_file="$HOME/$rel_path"
   if [ -L "$target_file" ]
   then
     # I should figure out how to properly backup a symlink
@@ -12,15 +12,14 @@ function backup_and_link {
   fi
   if [ -e "$target_file" ]
   then
-    # This should be fixed to point to the correct relative path
-    file_backup_dir="$backup_dir"
+    file_backup_dir="$(dirname "$backup_dir/$rel_path")"
     mkdir -p "$file_backup_dir"
     mv "$target_file" "$file_backup_dir"
   fi
+  mkdir -p $(dirname $target_file)
   ln -s "$source_file" "$target_file"
 }
 
-cd home
-find . -type f | while read f; do
+find "$dotfile_dir/home" -type f | while read f; do
   backup_and_link "$f"
 done
