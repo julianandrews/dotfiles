0b088594baf246f96e4cb830261bcd50b304aa53
Julian Andrews <jandrews@fusionbox.com>
2016-08-20 10:23:34 -0600

    Revert to simple xmonad layouts
    My fancy layouts have broken with recent versions.

--- home/.xmonad/lib/Layout.hs
+++ home/.xmonad/lib/Layout.hs
@@ -1,8 +1,9 @@
-module Layout (myTabbed, horizontalTabbed, verticalTabbed) where
+module Layout (myHorizontal, myVertical, myTabbed) where
 
 import XMonad
+import XMonad.Layout (Tall, Mirror)
 import XMonad.Layout.Decoration (Theme(..), defaultTheme, Decoration, DefaultShrinker)
-import XMonad.Layout.LayoutBuilder (LayoutN(..), layoutN, layoutAll, relBox)
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
 import XMonad.Layout.Tabbed (TabbedDecoration, tabbed, shrinkText)
 import XMonad.Util.Themes (ThemeInfo(..))
 
@@ -27,28 +28,12 @@ solarizedTheme = TI "Solarized Theme" "Julian Andrews" "Theme using Solarized's 
     }
   )
 
-horizontal :: (Eq a, Read a, Show a, Typeable a, LayoutClass l a) =>
-  l a -> Int -> Int -> Rational -> Rational -> LayoutN l (LayoutN l (LayoutN l Full)) a
-horizontal baseLayout n1 n2 r1 r2 =
-  layoutN n1 (relBox 0 0 r1 1) (Just $ relBox 0 0 1 1) baseLayout
-  $ layoutN n2 (relBox r1 0 1 r2) (Just $ relBox r1 0 1 1) baseLayout
-  $ layoutAll (relBox r1 r2 1 1) baseLayout
+type BaseTabbed = ModifiedLayout (Decoration TabbedDecoration DefaultShrinker) Simplest
 
-vertical :: (Eq a, Read a, Show a, Typeable a, LayoutClass l a) =>
-  l a -> Int -> Int -> Rational -> Rational -> LayoutN l (LayoutN l (LayoutN l Full)) a
-vertical baseLayout n1 n2 r1 r2 =
-  layoutN n1 (relBox 0 0 1 r1) (Just $ relBox 0 0 1 1) baseLayout
-  $ layoutN n2 (relBox 0 r1 r2 1) (Just $ relBox 0 r1 1 1) baseLayout
-  $ layoutAll (relBox r2 r1 1 1) baseLayout
+baseTabbed :: (Eq a, Read a) => BaseTabbed a
+baseTabbed = tabbed shrinkText (theme solarizedTheme)
 
-type MyTabbed = ModifiedLayout (Decoration TabbedDecoration DefaultShrinker) Simplest
-type MixedTabbed = LayoutN MyTabbed (LayoutN MyTabbed (LayoutN MyTabbed Full))
-
-myTabbed :: (Eq a, Read a) => MyTabbed a
-myTabbed = tabbed shrinkText (theme solarizedTheme)
-
-horizontalTabbed :: MixedTabbed Window
-horizontalTabbed = horizontal myTabbed 1 1 (3/5) (1/2)
-
-verticalTabbed :: MixedTabbed Window
-verticalTabbed = vertical myTabbed 1 1 (3/5) (1/2)
+myHorizontal = renamed [Replace "Horizontal"] $ Tall 1 (3/100) (3/5)
+myVertical = renamed [Replace "Vertical"] $ Mirror myHorizontal
+myTabbed :: (Eq a, Read a) => ModifiedLayout Rename BaseTabbed a
+myTabbed = renamed [Replace "Tabbed"] baseTabbed
--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -7,7 +7,6 @@ import XMonad.Hooks.EwmhDesktops (ewmh, fullscreenEventHook)
 import XMonad.Layout.Fullscreen (fullscreenManageHook)
 import XMonad.Layout.LayoutBuilder (IncLayoutN(..))
 import XMonad.Layout.NoBorders (lessBorders, Ambiguity(OtherIndicated))
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
 import XMonad.Util.EZConfig (additionalKeysP)
 
 import qualified XMonad.StackSet as W
@@ -50,12 +49,10 @@ myEventHook = composeAll [
     docksEventHook
   ]
 
-myLayout = renamed [Replace "Horizontal"] horizontalTabbed |||
-           renamed [Replace "Vertical"] verticalTabbed |||
-           renamed [Replace "Tabbed"] myTabbed
+myLayout = myHorizontal ||| myVertical ||| myTabbed
 
 myManageHook = composeAll [
-    role =? "gimp-image-window" --> (ask >>= doF . W.sink),
+    windowRole =? "gimp-image-window" --> (ask >>= doF . W.sink),
     fmap (isPrefixOf "Gimp-") className --> doFloat,
     className =? "Transmission-gtk" --> doFloat,
     fmap (isPrefixOf "Sgt-") className --> doFloat,
@@ -63,7 +60,7 @@ myManageHook = composeAll [
     fullscreenManageHook,
     manageHook defaultConfig
   ]
-  where role = stringProperty "WM_WINDOW_ROLE"
+  where windowRole = stringProperty "WM_WINDOW_ROLE"
 
 myKeys = [
     ("<XF86Sleep>", spawn "systemctl suspend"),
