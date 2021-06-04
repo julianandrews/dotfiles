22d69228744d254f2530bb44be2572abbbd25d82
Julian Andrews <jandrews@fusionbox.com>
2021-06-03 20:40:37 -0400

    Tweak gargantua-two xmobar indicator
    Now this displays a different status for failed connections.

--- .config/xmobar/gargantua-two-mail.sh
+++ .config/xmobar/gargantua-two-mail.sh
@@ -6,11 +6,16 @@ check_mail() {
   ssh "$1" '[ -s /var/mail/julian ] && echo mail'
 }
 
-if [ $(check_mail "$SERVER") ]
+mail=$(check_mail "$SERVER")
+if [ $? = 255 ]
 then
-  status='<fc=#b48900><fn=1> </fn></fc>'
-else
+  # ssh returns 255 on a failed connection
+  status='<fc=#cb4b16><fn=1> </fn></fc>'
+elif [ -z "$mail" ]
+then
   status='<fc=#268bd2>✓</fc>'
+else
+  status='<fc=#b48900><fn=1> </fn></fc>'
 fi
 
 echo "<action=\`urxvt -e ssh gargantua-two\`>G2$status</action>"
