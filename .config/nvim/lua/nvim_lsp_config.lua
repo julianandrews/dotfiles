e1102c48129ee68b5bafda0f7de4f81c6821b942
Julian Andrews <jandrews271@gmail.com>
2025-02-22 10:39:57 -0500

    Formatting fix for config

--- .config/nvim/lua/nvim_lsp_config.lua
+++ .config/nvim/lua/nvim_lsp_config.lua
@@ -60,9 +60,9 @@ end
 
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = true,
+    }
 )
