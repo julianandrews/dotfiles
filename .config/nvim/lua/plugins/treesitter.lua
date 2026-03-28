69e892ea766f1f0966a40d12370f9f453db91786
Julian Andews <jandrews271@gmail.com>
2026-03-28 10:49:13 -0400

    Add more neovim treesitter languages

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,19 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "rust", "typescript" },
+      ensure_installed = {
+    "lua",
+    "rust",
+    "typescript",
+    "python",
+    "bash",
+    "yaml",
+    "json",
+    "toml",
+    "cpp",
+    "c",
+    "dockerfile",
+  },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
