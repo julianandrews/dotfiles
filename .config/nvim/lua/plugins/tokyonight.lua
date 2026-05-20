cf0b2323a154f762c2874e5c9a1411d5059416ec
Julian Andews <jandrews271@gmail.com>
2026-05-20 12:00:48 -0400

    Enable diff highlighting in neovim
    I enabled diff highligting in treesitter, then tweaked the colors
    because the old bg-based highlights for git diffs were hard to read.

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,13 @@ return {
             lazy = false,
             priority = 1000,
             config = function()
+                require("tokyonight").setup({
+                    on_highlights = function(hl, c)
+                        hl.DiffAdd    = { fg = c.git.add,    bg = "NONE" }
+                        hl.DiffDelete = { fg = c.git.delete, bg = "NONE" }
+                        hl.DiffChange = { fg = c.git.change, bg = "NONE" }
+                    end,
+                })
                 vim.cmd.colorscheme("tokyonight-storm")
             end,
     },
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
     "cpp",
     "c",
     "dockerfile",
+    "diff",
   },
       auto_install = true,
       highlight = { enable = true },
