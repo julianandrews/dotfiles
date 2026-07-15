13018d0cab99ad2443f360c62d3c7a14521232a5
Julian Andrews <jandrews271@gmail.com>
2026-07-15 10:01:41 -0400

    Update treesitter for neovim 0.12
    Neovim 0.12 landed in debian testing and I don't need to be using the
    old "master" branch tree-sitter anymore (in fact, it broke).
    
    I've also removed `.config/nvim/lazy-lock.json` from dotfiles. Better
    not to track that I think.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master", -- Neccessary for neovim 0.11
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-    "lua",
-    "rust",
-    "typescript",
-    "python",
-    "bash",
-    "yaml",
-    "json",
-    "toml",
-    "cpp",
-    "c",
-    "dockerfile",
-    "diff",
-  },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+    require('nvim-treesitter').setup()
+
+    require('nvim-treesitter').install({
+      "lua", "rust", "typescript", "python", "bash", "yaml",
+      "json", "toml", "cpp", "c", "dockerfile", "diff",
+      "markdown", "markdown_inline",
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
